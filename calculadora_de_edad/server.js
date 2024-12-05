1ed470aef6eee6afe8935f1c9845d62e65cfe40a
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2024-12-04 21:21:10 -0300

    feat: Ejercicio 'calculadora de edad'

--- calculadora_de_edad/server.js
+++ calculadora_de_edad/server.js
@@ -2,7 +2,9 @@ const moment = require("moment");
 
 const fechaActual = moment().format("LLL");
 
-const fechaNacimiento = moment("2004-09-09", "YYYY-MM-DD");
+const fechaNacimiento = moment("2004-09-20", "YYYY-MM-DD");
+console.log(fechaNacimiento);
+
 const isValid = fechaNacimiento.isValid();
 
 if (isValid) {
@@ -11,5 +13,7 @@ if (isValid) {
   console.log("La fecha no es válida");
 }
 
+const cantDias = fechaNacimiento.diff() // no creo que esto este andando bien
 
-
+console.log(`Desde el día que naci hasta hoy pasaron: ${cantDias} dias`);
+console.log(365 * 20);
