fad9e018b6c1f091a0d616ed3295a01914b1e3aa
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2024-12-07 12:57:27 -0300

    feat: Función resta con promesas & then y catch

--- 01_hands_on_lab/server.js
+++ 01_hands_on_lab/server.js
@@ -1,4 +1,4 @@
-// suma es una funcion
+
 const suma = (nro1, nro2) => {
     return new Promise((resolve, reject) => {
         if (nro1 === 0 || nro2 === 0) {
@@ -7,18 +7,43 @@ const suma = (nro1, nro2) => {
         if (Math.sign(nro1 + nro2) === (-1)) {
             reject("La calculadora sólo debe devolver valores positivos")
 
-        }  
-        resolve(`El resultado es: ${nro1 + nro2}`) 
+        }
+        resolve(`El resultado es: ${nro1 + nro2}`)
     })
 }
 
-suma(1, 0)
-    .then((res) => { console.log("*", res); })
-    .catch((e) => { console.log("*", e); })
+/* suma(1, 0)
+    .then((res) => { console.log("Suma 1 -", res); })
+    .catch((e) => { console.log("Suma 1 -", e); })
 
 suma(1, -5)
-    .then((res) => { console.log("**", res); })
-    .catch((e) => { console.log("**", e); })
+    .then((res) => { console.log("Suma 2 -", res); })
+    .catch((e) => { console.log("Suma 2 -", e); })
 suma(1, 5)
-    .then((res) => { console.log("***", res); })
-    .catch((e) => { console.log("***", e); })
+    .then((res) => { console.log("Suma 3 -", res); })
+    .catch((e) => { console.log("Suma 3 -", e); }) */
+
+// Resta 
+const resta = (nro1, nro2) => {
+    return new Promise((resolve, reject) => {
+        if (nro1 === 0 || nro2 === 0) {
+            reject("Operación inválida")
+        }
+        if (Math.sign(nro1 - nro2) === (-1)) {
+            reject("La calculadora sólo debe devolver valores positivos")
+
+        }
+        resolve(`El resultado es: ${nro1 - nro2}`)
+    })
+}
+
+resta(1, 0)
+    .then((res) => { console.log("Resta 1 -", res); })
+    .catch((e) => { console.log("Resta 1 -", e); })
+
+resta(1, -5)
+    .then((res) => { console.log("Resta 2 -", res); })
+    .catch((e) => { console.log("Resta 2 -", e); })
+resta(1, 5)
+    .then((res) => { console.log("Resta 3 -", res); })
+    .catch((e) => { console.log("Resta 3 -", e); })
